cf2d5a796f60e843fcac5a7e98e0a14e0a59f6ec
Jumman5 <mhjummanan@gmail.com>
2021-04-15 00:16:43 +0200

    comment added at sum2.asm

--- Home_code/sum2.asm
+++ Home_code/sum2.asm
@@ -5,11 +5,11 @@
 	.word 11
 
 .text
-	lui s0, 0x10010
-	lw t0, 0(s0)
-	lw t1, 4(s0)
-	add a0, t0, t1
-	li a7, 1
+	lui s0, 0x10010 # also able to use la s0 , x/y 
+	lw t0, 0(s0)	#load first number from address
+	lw t1, 4(s0)	# load second number from address
+	add a0, t0, t1  # for printing return should be a0, a1
+	li a7, 1	# print	
 	ecall
-	li a7, 10
+	li a7, 10	#exit
 	ecall
